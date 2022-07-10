2d5b1db0f736f2ae7da4c2b69eccdc887b4866bf
Natalia Melo Fernandez <Natalia.MeloFernandez@softwire.com>
2022-07-10 17:27:50 +0100

    Ooriginal files

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,22 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for
-
-from todo_app.data.session_items import get_items, add_item
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config())
 
+
 @app.route('/')
 def index():
-    items = get_items()
-    return render_template('index.html', items = items)
-
-@app.route('/add_new_item', methods = ['POST'])
-def add_new_item():
-    title = request.form.get('title')
-    add_item(title)
-    return redirect(url_for('index'))
-
-
-
-
+    return 'Hello World!'
