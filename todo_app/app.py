daf26031de2029012c19fc05e66ee679493ac2ea
Natalia Melo Fernandez <Natalia.MeloFernandez@softwire.com>
2022-07-12 11:08:22 +0100

    Added Part 4

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,18 +1,25 @@
 from flask import Flask, render_template, request, redirect, url_for
 
+from todo_app.data.trello_items import fetch_all_cards, create_new_card, mark_as_complete
 from todo_app.data.session_items import get_items, add_item
 from todo_app.flask_config import Config
-from todo_app.data. import
 
 app = Flask(__name__)
 app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    items = get_items()
+    items = fetch_all_cards()
     return render_template('index.html', items = items)
 
 @app.route('/add_new_item', methods = ['POST'])
 def add_new_item():
-    add_item(request.form.get('title'))
+    create_new_card(request.form.get('title'))
+    return redirect(url_for('index'))
+
+
+@app.route('/complete_item/<id>')
+def complete_item(id):
+    print(id)
+    mark_as_complete(id)
     return redirect(url_for('index'))
--- todo_app/data/trello_items.py
+++ todo_app/data/trello_items.py
@@ -1,10 +1,47 @@
 import requests
 import os
 
-LIST_ID = [{'to-do-list_id': '62cc1c52bb382e686252622d'},
-           {'board' : '62cc1c1efb40f28f8a02eabb'}]
+# LIST_ID = [{'to-do-list_id': '62cc1c52bb382e686252622d'},
+           #{'board' : '62cc1c1efb40f28f8a02eabb'}]
 
-def fetch_all_cards(Board_ID):
+def fetch_all_cards():
+    r_json = json_of_all_cards()
+    return r_json
+    output_list = []
+    for item in r_json:
+        output_list.append(item['name'])
+
+    #return output_list
+
+def create_new_card(title):
+    # Board_ID = '62cc1c1efb40f28f8a02eabb'
+    url = 'https://api.trello.com/1/cards'
+
+    #query_parameters = {'key': '96232fa3a30653ad5263af0c8c1df055',
+                                #'token': '24fd9ec4f8fa09ab74fcc9c23da319308c1bf094ec9e69a4da7202768c0d6441'}
+
+    query = {'idList':'62cc1c1efb40f28f8a02eac3', 'key': '96232fa3a30653ad5263af0c8c1df055',
+             'token': '24fd9ec4f8fa09ab74fcc9c23da319308c1bf094ec9e69a4da7202768c0d6441','name':title}
+    headers = {
+        "Accept": "application/json"
+    }
+
+    r = requests.request("POST", url, params=query, headers=headers)
+
+
+def mark_as_complete(card_id):
+    query = {'idList':'62cc1c1efb40f28f8a02eac4', 'key': '96232fa3a30653ad5263af0c8c1df055',
+             'token': '24fd9ec4f8fa09ab74fcc9c23da319308c1bf094ec9e69a4da7202768c0d6441'}
+
+    url = 'https://api.trello.com/1/cards/'+card_id
+
+    headers = {
+        "Accept": "application/json"
+    }
+
+    r = requests.put(url, params=query, headers=headers)
+
+def json_of_all_cards():
     Board_ID = '62cc1c1efb40f28f8a02eabb'
     url = 'https://api.trello.com/1/boards/'+Board_ID + '/cards/open'
 
@@ -14,23 +51,15 @@ def fetch_all_cards(Board_ID):
     r = requests.get(url, params=query_parameters)
 
     r_json = r.json()
-
-    output_list = []
-    for item in r_json:
-        output_list.append(item['name'])
-
-    return output_list
-
-def create_new_card(title):
-    Board_ID = '62cc1c1efb40f28f8a02eabb'
-    to_do_list_id = '62cc1c52bb382e686252622d'
-    url = 'https://api.trello.com/1/boards/'+Board_ID + '/cards'
-
-    query = {'idList':'62cc1c52bb382e686252622d', 'key':os.environ.get('key'), 'token':os.environ.get('token'),
-             'name' : title}
-
-    r = requests.post(url, params=query)
-
-    return url
+    return r_json
 
 
+class Item:
+    def __init__(self, id, name, status = 'To Do'):
+        self.id = id
+        self.name = name
+        self.status = status \
+
+    @classmethod
+    def from_trello_card(cls, card, list):
+        return cls(card['id'], card['name'], list['name'])
