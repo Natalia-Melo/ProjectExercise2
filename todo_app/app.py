8bb030791637c8552171f544c03374787198fa04
Natalia Melo Fernandez <Natalia.MeloFernandez@softwire.com>
2022-07-12 14:18:30 +0100

    Removed unused class method

--- todo_app/app.py
+++ todo_app/app.py
@@ -36,7 +36,3 @@ class Item:
         self.idList = idList
         self.desc = desc
 
-    @classmethod
-    def from_trello_card(cls, card, list):
-        return cls(card['id'], card['name'], list['name'])
-
