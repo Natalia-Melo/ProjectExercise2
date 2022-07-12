a07cf97db4e59d7372c07415c278c99674dac4a3
Natalia Melo Fernandez <Natalia.MeloFernandez@softwire.com>
2022-07-12 14:32:05 +0100

    Modification to query parameters in trelli_items

--- todo_app/data/trello_items.py
+++ todo_app/data/trello_items.py
@@ -3,7 +3,8 @@ import os
 
 url_main_cards = 'https://api.trello.com/1/cards/'
 Board_ID = os.environ.get('BOARD_ID')
-
+base_query_parameters = {'key': os.environ.get('key'),
+                                'token': os.environ.get('token')}
 
 def fetch_all_cards():
     r_json = json_of_all_cards()
@@ -13,8 +14,10 @@ def create_new_card(title):
     url = url_main_cards
     data_todo_list = (define_lists_in_board())[0]
 
-    query = {'idList':data_todo_list['idList'], 'key': os.environ.get('key'),
-             'token': os.environ.get('token'),'name':title}
+    query = base_query_parameters
+    query.update({'name':title})
+    query.update({'idList':data_todo_list['idList']})
+
     headers = {
         "Accept": "application/json"
     }
@@ -23,8 +26,8 @@ def create_new_card(title):
 
 def mark_as_complete(card_id):
     data_done_list = (define_lists_in_board())[-1]
-    query = {'idList':data_done_list['idList'], 'key': os.environ.get('key'),
-             'token': os.environ.get('token')}
+    query = base_query_parameters
+    query.update({'idList':data_done_list['idList']})
 
     url = url_main_cards+card_id
 
@@ -36,8 +39,7 @@ def mark_as_complete(card_id):
 def json_of_all_cards():
     url = 'https://api.trello.com/1/boards/'+Board_ID + '/cards/open'
 
-    query_parameters = {'key': os.environ.get('key'),
-                                'token': os.environ.get('token')}
+    query_parameters = base_query_parameters
 
     r = requests.get(url, params=query_parameters)
 
@@ -47,8 +49,7 @@ def json_of_all_cards():
 def define_lists_in_board():
     url = 'https://api.trello.com/1/boards/'+Board_ID + '/lists/open'
 
-    query_parameters = {'key': os.environ.get('key'),
-                                'token': os.environ.get('token')}
+    query_parameters = base_query_parameters
 
     r = requests.get(url, params=query_parameters)
     r_json = r.json()
@@ -57,5 +58,4 @@ def define_lists_in_board():
     for item in r_json:
         lists_in_boards.append({'idList':item['id'], 'name':item['name']})
     return lists_in_boards
-
 
