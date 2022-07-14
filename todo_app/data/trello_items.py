3ecc1a0dbd5a1c394accca5b4f5d45c7b7c3619b
Natalia Melo Fernandez <Natalia.MeloFernandez@softwire.com>
2022-07-14 09:47:25 +0100

    Review 2 modifications

--- todo_app/data/trello_items.py
+++ todo_app/data/trello_items.py
@@ -4,7 +4,7 @@ import os
 Board_ID = os.environ.get('BOARD_ID')
 
 def build_url(endpoint):
-    return 'https://api.trello.com/1/'+endpoint
+    return f"https://api.trello.com/1/{endpoint}"
 
 def get_auth_params():
     return({'key':os.environ.get('KEY'), 'token':os.environ.get('TOKEN')})
@@ -13,10 +13,6 @@ def build_params(params = {}):
     full_params = get_auth_params()
     full_params.update(params)
     return full_params
-
-def fetch_all_cards():
-    r_json = json_of_all_cards()
-    return r_json
 
 def create_new_card(title):
     url = build_url('cards')
@@ -28,7 +24,7 @@ def create_new_card(title):
     requests.request("POST", url, params=query, headers=headers)
 
 def mark_as_complete(card_id):
-    url = build_url('cards/'+card_id)
+    url = build_url(f"cards/{card_id}")
     data_done_list = (define_lists_in_board())[-1]
     query = build_params({'idList':data_done_list['idList']})
     headers = {
@@ -36,19 +32,17 @@ def mark_as_complete(card_id):
     }
     requests.put(url, params=query, headers=headers)
 
-def json_of_all_cards():
-    url = build_url('boards/'+Board_ID + '/cards/open')
+def fetch_all_cards():
+    url = build_url(f"boards/{Board_ID}/cards/open")
     query = get_auth_params()
     r = requests.get(url, params=query)
     r_json = r.json()
     return r_json
 
 def define_lists_in_board():
-    url = build_url('boards/'+Board_ID + '/lists/open')
+    url = build_url(f"boards/{Board_ID}/lists/open")
     query = get_auth_params()
     r = requests.get(url, params=query)
     r_json = r.json()
-    lists_in_boards = []
-    for item in r_json:
-        lists_in_boards.append({'idList':item['id'], 'name':item['name']})
-    return lists_in_boards
+    return [{'idList':item['id'], 'name':item['name']} for item in r_json]
+
